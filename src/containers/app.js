b3329fe26bd31d23a58e24a3346a1466a765eb06
Caleb Kim <me@calebkim.me>
2017-02-04 15:57:58 -0800

    feat(App.js): Add navbar

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,6 +1,15 @@
 import React, { Component, PropTypes } from 'react';
-import { View } from 'react-native';
+import AppBar from 'material-ui/AppBar';
 import { connect } from 'react-redux';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { View } from 'react-native';
+
+import lifecycles from '../constants/lifecycles';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 class App extends Component {
   constructor(props) {
@@ -9,21 +18,33 @@ class App extends Component {
   }
 
   render() {
+    const { lifecycle } = this.props;
+    const title = lifecycle === lifecycles.AUTH_LOGGEDIN ? 'Home' : 'PolyRides';
     return (
-      <View>
-        { /* Add this for nested routes */ }
-        { this.props.children }
-      </View>
+      <MuiThemeProvider>
+        <View>
+          <AppBar title={title} />
+          { /* Add this for nested routes */ }
+          { this.props.children }
+        </View>
+      </MuiThemeProvider>
     );
   }
 }
 
 App.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.oneOfType([
+    React.PropTypes.arrayOf(React.PropTypes.node),
+    React.PropTypes.node,
+  ]),
+  lifecycle: PropTypes.string,
 };
 
-function mapStateToProps() {
-  return {};
+function mapStateToProps(state) {
+  const { auth: { lifecycle } } = state;
+  return {
+    lifecycle,
+  };
 }
 
 const mapDispatchToProps = {
