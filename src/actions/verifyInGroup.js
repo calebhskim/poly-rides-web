30bda37bbfddf7896d1995889cb3ddf93094e683
calebhskim <caleb.hs.kim@gmail.com>
2017-02-14 15:59:50 -0800

    Remove action

--- src/actions/verifyInGroup.js
+++
@@ -1,9 +0,0 @@
-import { push } from 'react-router-redux';
-
-import actions from '../constants/actions';
-
-export default function verifyInGroup() {
-  return (dispatch, getState) => {
-    const { firebase: { app }, user: { fbId } } = getState();
-  };
-}
