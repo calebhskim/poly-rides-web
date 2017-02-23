05c063164f7bd7e7c966538162ed3b629622620a
Lukas Froehlich <lukasfroehlich1@gmail.com>
2017-02-23 01:26:46 -0800

    a little more detail to sample test

--- tests/actions/setNavTitle.test.js
+++ tests/actions/setNavTitle.test.js
@@ -1,7 +1,7 @@
 import setNavTitle from '../../src/actions/setNavTitle';
 import types from '../../src/constants/actions';
 
-describe('actions', () => {
+describe('nav bar title action', () => {
   it('should change nav bar title', () => {
     const text = 'ridePage';
     const expectedAction = {
