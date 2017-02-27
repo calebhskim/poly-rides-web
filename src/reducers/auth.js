3d5ba94a2704eb339c1c6e0795b1e6476fc8a8a7
calebhskim <caleb.hs.kim@gmail.com>
2017-02-27 20:11:18 +0900

    Clean up auth reducer

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -28,38 +28,6 @@ const getUser = (payload) => {
 
 const auth = (state = initialState.auth, { payload, response, type }) => {
   switch(type) {
-    case actions.AUTH_LOGIN_START:
-      return Object.assign({}, state, {
-        lifecycle: lifecycles.AUTH_START,
-      });
-    case actions.AUTH_LOGIN_SUCCESS:
-      return Object.assign({}, state, {
-        error: {},
-        lifecycle: lifecycles.AUTH_LOGGEDIN,
-        user: {
-          ...state.user,
-          ...getUser(payload),
-        },
-      });
-    case actions.AUTH_LOGIN_FAILURE:
-      return Object.assign({}, state, {
-        error: payload,
-        lifecycle: lifecycles.AUTH_NOT_LOGGEDIN,
-      });
-    case actions.AUTH_SIGNUP_SUCCESS:
-      return Object.assign({}, state, {
-        error: {},
-        lifecycle: lifecycles.AUTH_LOGGEDIN,
-        user: {
-          ...state.user,
-          ...getUser(payload),
-        },
-      });
-    case actions.AUTH_SIGNUP_FAILURE:
-      return Object.assign({}, state, {
-        error: payload,
-        lifecycle: lifecycles.AUTH_NOT_LOGGEDIN,
-      });
     case actions.AUTH_SIGNOUT_SUCCESS:
       return Object.assign({}, state, {
         error: {},
