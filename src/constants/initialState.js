925d1083e909231d6f8f08930ba14e5c25912fc1
Lukas Froehlich <lukasfroehlich1@gmail.com>
2017-04-22 13:39:54 -0700

    component mostly done. autocomplete cities and get lat lng

--- src/constants/initialState.js
+++ src/constants/initialState.js
@@ -27,7 +27,9 @@ export default {
       displayCount: 10,
       isNextLoading: false,
       isPosting: '', // uid of current user used as boolean and id
+      isSearching: false, // in the middle of a search
       list: [],
+      tempList: [], // holds entries outside of search
       loadedRowsMap: {}, // True is loaded, false is loading
       startFrom: {}, // Object to be passed to startAt e.g. { key: '', timestamp: '' }
       totalCount: 0,
