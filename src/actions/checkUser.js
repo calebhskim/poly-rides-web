ce10c9a504707fc0865bee4b2fb416e66ccfe249
calebhskim <caleb.hs.kim@gmail.com>
2017-04-30 17:13:12 +0900

    Add more data to user

--- src/actions/checkUser.js
+++ src/actions/checkUser.js
@@ -1,16 +1,14 @@
 export default function checkUser() {
   return (dispatch, getState) => {
     const {
-      auth: { user: { fbId, uid } },
+      auth: { user },
       firebase: { app },
     } = getState();
     const userRef = app.database().ref(`users/${uid}`);
 
     userRef.once('value', (snapshot) => {
       if (snapshot.val() === null) {
-        userRef.set({
-          fbId,
-        });
+        userRef.set(user);
       }
     });
   };
