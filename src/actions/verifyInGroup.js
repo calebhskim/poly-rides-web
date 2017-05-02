94e92d8fff6e1a786e75e4548bd79a17d1e0ac59
Lukas Froehlich <lukasfroehlich1@gmail.com>
2017-05-02 13:29:31 -0700

    removed group verify

--- src/actions/verifyInGroup.js
+++ src/actions/verifyInGroup.js
@@ -1,24 +1,31 @@
 import { push } from 'react-router-redux';
 
 import actions from '../constants/actions';
-import signout from './signout';
+// import signout from './signout';
 
 export default function verifyInGroup() {
   return (dispatch, getState) => {
     const { firebase: { app }, auth: { user: { fbId } } } = getState();
     app.database().ref('members').child(fbId).once('value', (snapshot) => {
-      if(snapshot.val() !== null) {
-        dispatch({
-          type: actions.IN_FB_GROUP,
-        });
-        dispatch(push('/dashboard'));
-      } else {
-        dispatch({
-          type: actions.NOT_IN_FB_GROUP,
-        });
-        dispatch(signout());
-        dispatch(push('/'));
-      }
+      // disabled group verify to allow for testing
+      dispatch({
+        type: actions.IN_FB_GROUP,
+      });
+      dispatch(push('/dashboard'));
+
+
+      // if(snapshot.val() !== null) {
+      //   dispatch({
+      //     type: actions.IN_FB_GROUP,
+      //   });
+      //   dispatch(push('/dashboard'));
+      // } else {
+      //   dispatch({
+      //     type: actions.NOT_IN_FB_GROUP,
+      //   });
+      //   dispatch(signout());
+      //   dispatch(push('/'));
+      // }
     });
   };
 }
